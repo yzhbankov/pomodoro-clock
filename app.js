346385f53ad138a6b82ea981826dfd34e12395ea
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-11-13 16:06:32 +0200

    timer functionality

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var breakPlus = document.querySelector(".break-plus");
 var breakMinus = document.querySelector(".break-minus");
 
 taskPlus.addEventListener("click", function () {
-    var minus;
-    if (typeof Number(taskTime.innerText.slice(0, 2)) == "number") {
+    var minuts;
+    if (taskTime.innerText.slice(2, 3) == ":") {
         minuts = Number(taskTime.innerText.slice(0, 2)) + 1;
     } else {
         minuts = Number(taskTime.innerText.slice(0, 1)) + 1;
@@ -20,8 +20,8 @@ taskPlus.addEventListener("click", function () {
     clockTime.innerText = minuts + ":00";
 });
 taskMinus.addEventListener("click", function () {
-    var minus;
-    if (typeof Number(taskTime.innerText.slice(0, 2)) == "number") {
+    var minuts = 0;
+    if (taskTime.innerText.slice(2, 3) == ":") {
         minuts = Number(taskTime.innerText.slice(0, 2)) - 1;
     } else if (Number(taskTime.innerText.slice(0, 1)) >= 1) {
         minuts = Number(taskTime.innerText.slice(0, 1)) - 1;
@@ -31,7 +31,7 @@ taskMinus.addEventListener("click", function () {
 });
 breakPlus.addEventListener("click", function () {
     var minuts;
-    if (typeof Number(breakTime.innerText.slice(0, 2)) == "number") {
+    if (breakTime.innerText.slice(2, 3) == ":") {
         minuts = Number(breakTime.innerText.slice(0, 2)) + 1;
     } else {
         minuts = Number(breakTime.innerText.slice(0, 1)) + 1;
@@ -39,12 +39,83 @@ breakPlus.addEventListener("click", function () {
     breakTime.innerText = minuts + ":00";
 });
 breakMinus.addEventListener("click", function () {
-    if (Number(breakTime.innerText.slice(0, 1)) >= 1) {
-        var minuts = Number(breakTime.innerText.slice(0, 1)) - 1;
-        breakTime.innerText = minuts + ":00";
+    var minuts = 0;
+    if (breakTime.innerText.slice(2, 3) == ":") {
+        minuts = Number(breakTime.innerText.slice(0, 2)) - 1;
+    } else if (Number(breakTime.innerText.slice(0, 1)) >= 1) {
+        minuts = Number(breakTime.innerText.slice(0, 1)) - 1;
     }
-    if (typeof Number(breakTime.innerText.slice(0, 2)) == "number") {
-        var minuts = Number(breakTime.innerText.slice(0, 2)) - 1;
-        breakTime.innerText = minuts + ":00";
-    }
+    breakTime.innerText = minuts + ":00";
 });
+var switcher = 1;
+var timeOfBreak = false;
+
+clockTime.addEventListener("click", function () {
+    switcher++;
+
+    timer(clockTime.innerText);
+    /*    var minuts = Number(time(clockTime.innerText)[0]);
+     var seconds = Number(time(clockTime.innerText)[1]);
+     var timerId = setInterval(function () {
+     if (((minuts == 0) && (seconds == 0)) || (switcher % 2 != 0)) {
+     clearInterval(timerId);
+     } else {
+     if (seconds == 0) {
+     minuts--;
+     seconds = 59;
+     } else {
+     seconds--;
+     }
+     clockTime.innerText = minuts + ":" + seconds;
+     }
+     }, 1000);*/
+});
+
+function time(time) {
+    var arr = [];
+    if (time.length == 5) {
+        var minuts = time.slice(0, 2);
+        var seconds = time.slice(3, 5);
+    } else {
+        var minuts = time.slice(0, 1);
+        var seconds = time.slice(2, 4);
+    }
+    arr.push(minuts);
+    arr.push(seconds);
+    return arr;
+}
+
+
+function timer(duration) {
+
+    var minuts = Number(time(duration)[0]);
+    var seconds = Number(time(duration)[1]);
+    var timerId = setInterval(function () {
+        if (switcher % 2 != 0) {
+            clearInterval(timerId);
+        } else if ((minuts == 0) && (seconds == 0)) {
+            clearInterval(timerId);
+            timeOfBreak = !timeOfBreak;
+            if (!timeOfBreak) {
+                clockTime.innerText = taskTime.innerText;
+            } else {
+                clockTime.innerText = breakTime.innerText;
+            }
+            timer(clockTime.innerText);
+        }
+        else {
+            if (seconds == 0) {
+                minuts--;
+                seconds = 59;
+            } else {
+                seconds--;
+            }
+            clockTime.innerText = minuts + ":" + seconds;
+        }
+    }, 1000);
+
+    if (switcher % 2 != 0) {
+        clearInterval(timerId);
+        console.log(switcher);
+    }
+}
